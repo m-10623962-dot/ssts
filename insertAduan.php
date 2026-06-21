f1feb44ce90617a3038d6a2dd4de610f1c2df590
m-10623962-dot <m-10623962@moe-dl.edu.my>
2026-06-21 16:04:40 +0800

    Add files via upload

---
+++ insertAduan.php
@@ -0,0 +1,16 @@
+<?PHP include 'connection.php';
+if(isset($_POST['submit'])) {
+$perkara = $_POST['perkara'];
+$aduan = $_POST['aduan'];
+$insertQuery = "INSERT INTO aduan (perkara, aduan) VALUES
+('$perkara','$aduan')";
+if (mysqli_query($conn, $insertQuery)) {
+echo "Successful";
+}
+else {
+echo "Something is wrong".$insertQuery."".mysqli_error($conn);
+}
+mysqli_error($conn);
+}
+?>
+</body?>
